6bd0cd37f5c98238abb63b92a80ef1769ef643fb
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-06-11 04:22:13 -0300

    Removed Hyperlinked

--- python_server/tpm/tpm_api/serializers.py
+++ python_server/tpm/tpm_api/serializers.py
@@ -2,7 +2,7 @@ from .models import Dates
 from rest_framework import serializers
 
 
-class DatesSerializer(serializers.HyperlinkedModelSerializer):
+class DatesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dates
         fields = "__all__"
