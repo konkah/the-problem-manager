07919904924b517f7b406332f6950f634fed0a7d
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-06-17 00:29:59 -0300

    fixed file

--- the_problem_manager/lib/controller/manage_db/manage_db_state.dart
+++ the_problem_manager/lib/controller/manage_db/manage_db_state.dart
@@ -1,1 +1,23 @@
-class ManageState {  String message;  ManageState({this.message});}class InsertState extends ManageState {  InsertState() : super(message: "Dados inseridos com sucesso!");}class DeleteState extends ManageState {  DeleteState() : super(message: "Dados removidos com sucesso!");}class ErrorState extends ManageState {  ErrorState(String message) : super(message: message);}class LoginState extends ManageState {  bool worked;  String email;  String password;  LoginState({this.worked, this.email, this.password}) : super(message: "Dados inconsistentes");}
+class ManageState {
+  String message;
+  ManageState({this.message});
+}
+
+class InsertState extends ManageState {
+  InsertState() : super(message: "Dados inseridos com sucesso!");
+}
+
+class DeleteState extends ManageState {
+  DeleteState() : super(message: "Dados removidos com sucesso!");
+}
+
+class ErrorState extends ManageState {
+  ErrorState(String message) : super(message: message);
+}
+
+class LoginState extends ManageState {
+  bool worked;
+  String email;
+  String password;
+  LoginState({this.worked, this.email, this.password}) : super(message: "Dados inconsistentes");
+}
